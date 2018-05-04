338d16e815111bcb612f017e02c1d6946b1df935
Sreno1 <slipvana101@gmail.com>
2018-05-04 14:55:55 -0400

    Task Automations & Resource Allocation
    - added upgradeable memory
    - added basic task automation 
    - TODO: correct dynamic CPU resource allocation based on +/- buttons

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ var execute = 0;
 var mem = 0;
 var wb = 0;
 
-var cpu = 100;
+var cpu = 32;
 var disk = 4;
-var memory = 1;
+var memory = 16;
 var network = 0;
 var cpuUsed = 0;
 var diskUsed = 0;
@@ -22,10 +22,10 @@ var networkUsed = 0;
 var cores = 1;
 
 var diskCost = 4;
-var memoryCost = 64;
+var memoryCost = 48;
 var networkCost = 256;
-var cpuCost = 128;
-var moduleCost = 24;
+var cpuCost = 96;
+var moduleCost = 16;
 var coreCost = 512;
 
 var diskUpgrades = 0;
@@ -34,6 +34,13 @@ var networkUpgrades = 0;
 var cpuUpgrades = 0;
 var coreUpgrades = 0;
 var moduleUpgrades = 0;
+
+var commandOutputs = 11;
+
+var blockAutomation = 0;
+var blockAutomationSpeed = 5000;
+var blockAutomationTimeout = 1000;
+var blockAutomationTime = 5;
 
 var powerCallback = function() {
   powered = 1;
@@ -54,94 +61,123 @@ var powerCallback = function() {
       $("#nameInput").fadeIn( "slow", function() {});
       $( "#name" ).focus();
     }, 5000);
-    $(document).keypress(function() {
-        if (event.which == 13){
-          gameLoop();
-        }
-    });
-    setTimeout(
-    function()
-    {
-      $(document).keypress(function() {
-          if (event.which == 13){
-            gameLoop();
-          }
-      });
-    }, 10000);
+    $(document).keypress(function(e){
+      var checkWebkitandIE=(e.which==13 ? 1 : 0);
+      var checkMoz=(e.which==13 ? 1 : 0);
+
+      if (checkWebkitandIE || checkMoz)
+        gameLoop();
+  });
   };
 };
 
 function begin(){
-  $(document).keypress(function() {
-      if (event.which == 13) powerCallback();
-  });
+  $(document).keypress(function(e){
+    var checkWebkitandIE=(e.which==13 ? 1 : 0);
+    var checkMoz=(e.which==13 ? 1 : 0);
 
+    if (checkWebkitandIE || checkMoz)
+      powerCallback();
+});
   $('#powerOn').click(powerCallback);
 };
 
 
 
 function gameLoop(){
+  if($("#name").val() != "")
+    $("#commanderName").html($("#name").val());
+  else {
+    $("#commanderName").html("sample-name");
+  }
   $("#prompt").fadeOut( "slow", function() {});
   $("#nameInput").fadeOut( "slow", function() {});
   $( "#nameInput" ).remove();
   $( "#prompt" ).remove();
   $( ".center" ).remove();
-$("#pipeline").fadeIn( "slow", function() {});
-$("#gameContainer").fadeIn( "slow", function() {});
-$("#diskPercent").html("0%");
-$("#cpuPercent").html("0%");
-$("#memoryPercent").html("0%");
-$("#networkPercent").html("0%");
-$("#blocksAvailable").html(availableBlocks);
-$("#blocksCreated").html(createdBlocks);
-$("#blocksProcessed").html(processedBlocks);
-$("#diskCost").html(diskCost);
-$("#moduleCost").html(moduleCost);
-$("#coreCost").html(coreCost);
-$("#networkCost").html(networkCost);
-$("#cpuCost").html(cpuCost);
-$("#memoryCost").html(memoryCost);
+  $("#pipeline").fadeIn( "slow", function() {});
+  $("#gameContainer").fadeIn( "slow", function() {});
+  $("#diskPercent").html("0%");
+  $("#cpuPercent").html("0%");
+  $("#memoryPercent").html("0%");
+  $("#networkPercent").html("0%");
+  $("#blocksAvailable").html(availableBlocks);
+  $("#blocksCreated").html(createdBlocks);
+  $("#blocksProcessed").html(processedBlocks);
+  $("#diskCost").html(diskCost);
+  $("#moduleCost").html(moduleCost);
+  $("#coreCost").html(coreCost);
+  $("#networkCost").html(networkCost);
+  $("#cpuCost").html(cpuCost);
+  $("#memoryCost").html(memoryCost);
+  $("#blockStatus").html("OFF");
 
 
-  $(document).keypress(function() {
-    console.log(event.which);
+  $("#commandLine").on('keyup', function(e) {
+    if(e.keyCode==13){
+      var str = $("#commandLine").val();
+      var change = str.split(" ", 2);
+      if(change[0] == "disk"){
+         commandOutput("disk: " + diskUsed.toString() + "/" + disk.toString());
+      }
+      else if(change[0] == "blocks"){
+         availableBlocks = parseInt(change[1]);
+         $("#blocksAvailable").html(availableBlocks);
+         commandOutput(str);
+      }
+      $("#commandLine").val("");
+      return false;
+    }
+  });
+  Mousetrap.bind('f', function(){
     if (!$('#commandLine').is(':focus')) {
-      if (event.which == 102 && firstInstruction == 1 && cycleBlocked != 1){
+      if (firstInstruction == 1 && cycleBlocked != 1){
         fetch = 1;
         $("#fetch").css('color','#cfcfd3')
         firstInstruction = 0;
       }
-      if (event.which == 102 && wb == 1 && cycleBlocked != 1){
+      if (wb == 1 && cycleBlocked != 1){
         fetch = 1;
         wb = 0;
         $("#fetch").css('color','#cfcfd3')
       }
-      if (event.which == 102 && cycleBlocked != 1){
-        //log blocked cycle into terminal
-      }
-
-      if (event.which == 103 && fetch == 1){
+    }
+  });
+  Mousetrap.bind('g', function(){
+    if (!$('#commandLine').is(':focus')) {
+      if (fetch == 1){
         decode = 1;
         fetch = 0;
         $("#fetch").css('color', 'grey')
         $("#decode").css('color','#cfcfd3')
       }
-      if (event.which == 104 && decode == 1){
+    }
+  });
+  Mousetrap.bind('h', function(){
+    if (!$('#commandLine').is(':focus')) {
+      if (decode == 1){
         execute = 1;
         decode = 0;
         $("#decode").css('color', 'grey')
         $("#execute").css('color','#cfcfd3')
       }
-      if (event.which == 106 && execute == 1){
+    }
+  });
+  Mousetrap.bind('j', function(){
+    if (!$('#commandLine').is(':focus')) {
+      if (execute == 1){
         mem = 1;
         execute = 0;
         $("#execute").css('color', 'grey')
         $("#mem").css('color','#cfcfd3')
       }
-      if (event.which == 107 && mem == 1){
-        $("#mem").css('color', 'grey')
+    }
+  });
+  Mousetrap.bind('k', function(){
+    if (!$('#commandLine').is(':focus')) {
+      if (mem == 1){
         if(diskUsed < disk){
+          $("#mem").css('color', 'grey')
           $("#wb").toggleClass('cycleFinished')
           setTimeout(
           function()
@@ -161,98 +197,192 @@ $("#memoryCost").html(memoryCost);
           {
             $("#wb").toggleClass('cycleBlocked')
           }, 700);
-          //log storage error
-        }
-      }
-      if (event.which == 97){
-        if(availableBlocks >= moduleCost){
-          $("#moduleUpgrade").toggleClass('sufficient');
-          setTimeout(
-          function()
-          {
-            $("#moduleUpgrade").toggleClass('sufficient')
-          }, 700);
-          moduleUpgrades++;
-          console.log(moduleUpgrades);
-          availableBlocks= availableBlocks-moduleCost;
-          processedBlocks+=moduleCost;
-          $("#blocksProcessed").html(processedBlocks);
-          $("#blocksAvailable").html(availableBlocks);
-          moduleCost = moduleCost*4;
-          $("#moduleCost").html(moduleCost);
-          if(moduleUpgrades == 1){
-            $("#taskAutomations").css("visibility", "visible");
-          }
-          else if(moduleUpgrades == 2){
-            $("#resourceAllocation").css("visibility", "visible");
-            //enable block-producing opportunities from connections
-          }
-          else if(moduleUpgrades == 3){
-            $("#connections").css("visibility", "visible");
-            $("#networkPercent").css("visibility", "visible")
-            //enable block-producing opportunities from connections
-          }
-          else if(moduleUpgrades == 4){
-            $("#assets").css("visibility", "visible");
-            //enable money-making opportunities from connections
-          }
-          else if(moduleUpgrades == 5){
-            $("#researchDevelopment").css("visibility", "visible");
-          }
-          else if(moduleUpgrades == 6){
-            $("#realmStatus").css("visibility", "visible");
-            $("#newsTicker").css("visibility", "visible");
-            $("#moduleUpgrade").css("visibility", "hidden");
-          }
-          //log upgrade in console
-        }
-        else{
-          $("#moduleUpgrade").toggleClass('insufficient');
-          setTimeout(
-          function()
-          {
-            $("#moduleUpgrade").toggleClass('insufficient')
-          }, 700);
-          //log not enough blocks
-        }
-      }
-      if (event.which == 100){
-        if(availableBlocks >= diskCost){
-          $("#diskUpgrade").toggleClass('sufficient');
-          setTimeout(
-          function()
-          {
-            $("#diskUpgrade").toggleClass('sufficient')
-          }, 700);
-          diskUpgrades++;
-          availableBlocks= availableBlocks-diskCost;
-          processedBlocks+=diskCost;
-          $("#blocksProcessed").html(processedBlocks);
-          $("#blocksAvailable").html(availableBlocks);
-          if(diskUpgrades == 2){
-            $("#memoryUpgrade").css("visibility", "visible");
-            $("#cpuUpgrade").css("visibility", "visible");
-            $("#moduleUpgrade").css("visibility", "visible");
-          }
-          disk = disk*4;
-          diskCost = diskCost*3;
-          $("#diskCost").html(diskCost);
-          $("#diskPercent").html((diskUsed/disk*100).toFixed(0) + "%");
-          //log upgrade in console
-        }
-        else{
-          $("#diskUpgrade").toggleClass('insufficient');
-          setTimeout(
-          function()
-          {
-            $("#diskUpgrade").toggleClass('insufficient')
-          }, 700);
-          //log not enough blocks
+          commandOutput("CYCLE BLOCKED (NOT ENOUGH STORAGE)")
         }
       }
     }
-    else{
-      if (event.which == 13) command();
+  });
+  Mousetrap.bind('a', function(){
+    if (!$('#commandLine').is(':focus')) {
+      if(availableBlocks >= moduleCost){
+        $("#moduleUpgrade").toggleClass('sufficient');
+        setTimeout(
+        function()
+        {
+          $("#moduleUpgrade").toggleClass('sufficient')
+        }, 700);
+        moduleUpgrades++;
+        availableBlocks= availableBlocks-moduleCost;
+        processedBlocks+=moduleCost;
+        diskUsed = availableBlocks;
+        $("#blocksProcessed").html(processedBlocks);
+        $("#blocksAvailable").html(availableBlocks);
+        moduleCost = moduleCost*2;
+        $("#moduleCost").html(moduleCost);
+        if(moduleUpgrades == 1){
+          $("#taskAutomations").css("visibility", "visible");
+        }
+        else if(moduleUpgrades == 2){
+          $("#resourceAllocation").css("visibility", "visible");
+          $("#memoryUpgrade").css("visibility", "visible");
+          $("#cpuUpgrade").css("visibility", "visible");
+        }
+        else if(moduleUpgrades == 3){
+          $("#connections").css("visibility", "visible");
+          $("#networkPercent").css("visibility", "visible")
+          $("#allocateNetwork").css("visibility", "visible")
+          //enable block-producing opportunities from connections
+        }
+        else if(moduleUpgrades == 4){
+          $("#assets").css("visibility", "visible");
+          //enable money-making opportunities from real world connections
+        }
+        else if(moduleUpgrades == 5){
+          $("#researchDevelopment").css("visibility", "visible");
+        }
+        else if(moduleUpgrades == 6){
+          $("#realmStatus").css("visibility", "visible");
+          $("#newsTicker").css("visibility", "visible");
+          $("#moduleUpgrade").css("visibility", "hidden");
+        }
+        commandOutput("NEW MODULE ADDED");
+      }
+      else{
+        $("#moduleUpgrade").toggleClass('insufficient');
+        setTimeout(
+        function()
+        {
+          $("#moduleUpgrade").toggleClass('insufficient')
+        }, 700);
+        commandOutput("NOT ENOUGH BLOCKS IN STORAGE");
+      }
+    }
+  });
+  Mousetrap.bind('d', function(){
+    if (!$('#commandLine').is(':focus')) {
+      if(availableBlocks >= diskCost){
+        $("#diskUpgrade").toggleClass('sufficient');
+        setTimeout(
+        function()
+        {
+          $("#diskUpgrade").toggleClass('sufficient')
+        }, 700);
+        diskUpgrades++;
+        availableBlocks= availableBlocks-diskCost;
+        processedBlocks+=diskCost;
+        diskUsed = availableBlocks;
+        $("#blocksProcessed").html(processedBlocks);
+        $("#blocksAvailable").html(availableBlocks);
+        disk = disk*4;
+        diskCost = diskCost*3;
+        $("#diskCost").html(diskCost);
+        $("#diskPercent").html((diskUsed/disk*100).toFixed(0) + "%");
+        commandOutput("DISK SIZE UPGRADED");
+      }
+      else{
+        $("#diskUpgrade").toggleClass('insufficient');
+        setTimeout(
+        function()
+        {
+          $("#diskUpgrade").toggleClass('insufficient')
+        }, 700);
+        commandOutput("NOT ENOUGH BLOCKS IN STORAGE");
+      }
+    }
+  });
+  Mousetrap.bind('m', function(){
+    if (!$('#commandLine').is(':focus')) {
+      if(availableBlocks >= memoryCost){
+        $("#memoryUpgrade").toggleClass('sufficient');
+        setTimeout(
+        function()
+        {
+          $("#diskUpgrade").toggleClass('sufficient')
+        }, 700);
+        memoryUpgrades++;
+        availableBlocks= availableBlocks-memoryCost;
+        processedBlocks+=memoryCost;
+        diskUsed = availableBlocks;
+        $("#blocksProcessed").html(processedBlocks);
+        $("#blocksAvailable").html(availableBlocks);
+        memory = memory*2;
+        memoryCost = memoryCost*3;
+        $("#memoryCost").html(memoryCost);
+        $("#memoryPercent").html((diskUsed/disk*100).toFixed(0) + "%");
+        commandOutput("MEMORY CAPACITY UPGRADED");
+      }
+      else{
+        $("#memoryUpgrade").toggleClass('insufficient');
+        setTimeout(
+        function()
+        {
+          $("#memoryUpgrade").toggleClass('insufficient')
+        }, 700);
+        commandOutput("NOT ENOUGH BLOCKS IN STORAGE");
+      }
+    }
+  });
+  Mousetrap.bind('c', function(){
+    if (!$('#commandLine').is(':focus')) {
+      if(blockAutomation == 0){
+        blockAutomation = 1;
+        cpuUsed += 16;
+        $("#diskPercent").html((cpuUsed/cpu*100).toFixed(0) + "%");
+        $("#blockStatus").html("ON");
+        $("#blockTime").html(blockAutomationTime + "s");
+          var blockAutomationLoop = setInterval(function() {
+            if (blockAutomation == 0){
+              clearInterval(blockAutomationLoop);
+            }
+            setTimeout(function() {
+              $("#fetch").css('color','#cfcfd3')
+              setTimeout(function() {
+                $("#fetch").css('color', 'grey')
+                $("#decode").css('color','#cfcfd3')
+                setTimeout(function() {
+                  $("#decode").css('color', 'grey')
+                  $("#execute").css('color','#cfcfd3')
+                  setTimeout(function() {
+                    $("#execute").css('color', 'grey')
+                    $("#mem").css('color','#cfcfd3')
+                    mem = 1;
+                  }, blockAutomationTimeout);
+                }, blockAutomationTimeout);
+              }, blockAutomationTimeout);
+            }, blockAutomationTimeout);
+            $("#mem").css('color', 'grey')
+            if(diskUsed < disk){
+              $("#wb").toggleClass('cycleFinished')
+              setTimeout(
+              function()
+              {
+                $("#wb").toggleClass('cycleFinished')
+              }, 700);
+              createBlock();
+              cycleBlocked = 0;
+            }
+            else{
+              cycleBlocked = 1;
+              $("#wb").toggleClass('cycleBlocked')
+              setTimeout(
+              function()
+              {
+                $("#wb").toggleClass('cycleBlocked')
+              }, 700);
+              commandOutput("CYCLE BLOCKED (NOT ENOUGH STORAGE)")
+            }
+          }, blockAutomationSpeed);
+      }
+      else{
+        blockAutomation = 0;
+        $("#blockStatus").html("OFF");
+        $("#blockTime").html("");
+        fetch = 0;
+        decode = 0;
+        execute = 0;
+        mem = 1;
+        wb = 0;
+      }
     }
   });
 };
@@ -266,20 +396,45 @@ function createBlock(){
     $("#diskPercent").html((diskUsed/disk*100).toFixed(0) + "%");
 };
 
-function command(){
-  var str = $("#commandLine").val();
-   var change = str.split(" ", 2);
-   if(change[0] == "disk"){
-      disk = parseInt(change[1]);
-      $("#diskPercent").html((diskUsed/disk*100).toFixed(0) + "%");
-   }
-   else if(change[0] == "blocks"){
-      availableBlocks = parseInt(change[1]);
-      $("#blocksAvailable").html(availableBlocks);
-   }
-   else{
-     $("#output").html("command not found");
-   }
-   $("#commandLine").val("");
+function commandOutput(outputString){
+  commandOutputs--;
 
-}
+    $("#output1").html($("#output2").html());
+    $("#output2").html($("#output3").html());
+    $("#output3").html($("#output4").html());
+    $("#output4").html($("#output5").html());
+    $("#output5").html($("#output6").html());
+    $("#output6").html($("#output7").html());
+    $("#output7").html($("#output8").html());
+    $("#output8").html($("#output9").html());
+    $("#output9").html($("#output10").html());
+    $("#output10").html(outputString);
+
+};
+
+function detractCPUAllocation(){
+  if (blockAutomation == 1){
+    if (blockAutomationSpeed > 1000){
+      blockAutomationSpeed -= 1000;
+      blockAutomationTimeout -= 200;
+      blockAutomationTime -= 1;
+      cpuUsed = cpuUsed/2;
+      $("#blockTime").html(blockAutomationTime + "s");
+    }
+    else if (blockAutomationTime > 4999){
+      commandOutput("MINIMUM CPU USAGE");
+    }
+    else{
+      //divide block automation time by 2
+    }
+  }
+  else{
+    commandOutput("NO AUTOMATIONS RUNNING");
+  }
+
+};
+
+function addCPUAllocation(){
+
+
+};
